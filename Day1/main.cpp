0eeeb240eedb8bd3e6403aedfc37685bdb53bd73
k1m190r <k1m190r@outlook.com>
2023-12-08 01:00:50 +0100

    working! 55029

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -1,20 +1,26 @@
 #include <fmt/format.h>
 #include <fstream>
 #include <string>
+#include <vector>
 
-using std::string, fmt::print, std::istringstream;
-using std::ifstream, std::ios_base, std::getline;
+using fmt::print, std::ifstream, std::getline, std::string, std::vector;
 
-int main(){
-    ifstream infile{"input1"};
+int main() {
+  ifstream infile{"input1"};
 
-    for( string line; getline(infile, line); ){
-        // print("{}:{}\n", line, line.size() == line.length());
+  int sum{0};
 
-        char const *c = line.c_str();
-        size_t len = line.length();
+  for (string line; getline(infile, line);) {
+    vector<char> nums{};
 
-        print("{} ", len);
+    // 0x30 .. 0x39 = 0 .. 9
+    for (const char c : line)
+      if ((c >= 0x30) && (c <= 0x39)) nums.push_back(c);
 
-    }
+    int v1 = nums[0] - 0x30;
+    int v2 = nums.back() - 0x30;
+    int v = v1 * 10 + v2;
+    sum += v;
+  }
+  print("sum: {}\n", sum);
 }
