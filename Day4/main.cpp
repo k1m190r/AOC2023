9eac430fb120bed5b86f226faffb5128b3f9f1c7
k1m190r <k1m190r@outlook.com>
2023-12-11 20:30:19 +0100

    Day 4 start

---
+++ Day4/main.cpp
@@ -0,0 +1,27 @@
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <assert.h>
+#include <map>
+#include <tuple>
+#include <ranges>
+#include <algorithm>
+#include "../demangle.h"
+
+using fmt::print, fmt::format;
+using std::ifstream, std::getline;
+using std::stringstream, std::string;
+using std::vector, std::map, std::tuple;
+
+
+
+int main() {
+  ifstream infile{"input_test_4"};
+  // input3_test_4 = 
+  // ifstream infile{"input"};
+  // input3 = 
+
+}
